0a628f6f311baddb7a166ad3722d67709670b893
kenrube <kenrube@gmail.com>
2021-12-06 23:57:40 +0800

    #70 Refactor shaders code

--- app/src/main/res/raw/fragment_solarize.glsl
+++ app/src/main/res/raw/fragment_solarize.glsl
@@ -1,17 +1,17 @@
 varying highp vec2 textureCoordinate;
 
 uniform sampler2D inputImageTexture;
-uniform highp float threshold;
+uniform lowp float threshold;
 
 // Values from "Graphics Shaders: Theory and Practice" by Bailey and Cunningham
 const mediump vec3 luminanceWeighting = vec3(0.2125, 0.7154, 0.0721);
 
 void main()
 {
-    highp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
-    highp float luminance = dot(textureColor.rgb, luminanceWeighting);
-    highp float thresholdResult = step(luminance, threshold);
-    highp vec3 finalColor = abs(thresholdResult - textureColor.rgb);
+    lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
+    lowp float luminance = dot(textureColor.rgb, luminanceWeighting);
+    lowp float thresholdResult = step(luminance, threshold);
+    lowp vec3 finalColor = abs(thresholdResult - textureColor.rgb);
     
-    gl_FragColor = vec4(finalColor, textureColor.w);
+    gl_FragColor = vec4(finalColor, textureColor.a);
 }
